1a74396fff69fedbd74c6766584036e23d0dffa6
Kyle Mellander <kmellander@focusvision.com>
2016-12-07 10:59:33 -0800

    Add day2 stuff

--- lib/day2/1.ex
+++ lib/day2/1.ex
@@ -1,10 +1,104 @@
 defmodule Day2 do
   def leg1 do
-
+    input = parse_input(ReadInput.from_day(2))
+    code_list(5, input)
+      |> Enum.join("")
   end
 
-  def parse_input do
-    ReadInput.from_day(2)
-      |> String.split("\n")
+  def leg2 do
+    parse_input(ReadInput.from_day(2))
+      |> base13_code(5)
+      |> Enum.map(fn(x) -> letterize(x) end)
+      |> Enum.join("")
+  end
+
+  def code_list(start, input) do
+    { result, _ } = Enum.map_reduce(input, start, fn(directions, position) ->
+      new_number = next_number(position, directions)
+      { new_number, new_number }
+    end)
+
+    result
+  end
+
+  def letterize(x) do
+    case x do
+      10 -> "A"
+      11 -> "B"
+      12 -> "C"
+      13 -> "D"
+      _ -> x
+    end
+  end
+
+  def base13_code(input, start) do
+    { result, _ } = Enum.map_reduce(input, start, fn(directions, position) ->
+      new_number = next_base13_number(position, directions)
+      { new_number, new_number }
+    end)
+
+    result
+  end
+
+  def next_number(start, directions) do
+    Enum.reduce(directions, start, fn(direction, position) ->
+      move(position, direction)
+    end)
+  end
+
+  def next_base13_number(start, directions) do
+    Enum.reduce(directions, start, fn(direction, position) ->
+      move13(position, direction)
+    end)
+  end
+
+  def move(position, direction) do
+    case direction do
+      "U" ->
+        if position - 3 > 0, do: position - 3, else: position
+      "D" ->
+        if position + 3 < 10, do: position + 3, else: position
+      "R" ->
+        if rem(position, 3) == 0, do: position, else: position + 1
+      "L" ->
+        if rem(position - 1, 3) == 0, do: position, else: position - 1
+      _ ->
+        position
+    end
+  end
+
+  def move13(position, direction) do
+    case direction do
+      "U" ->
+        cond do
+          Enum.member?([3, 13], position) ->
+            position - 2
+          5 < position && rem(position - 1, 4) != 0 ->
+            position - 4
+          true -> position
+        end
+      "D" ->
+        cond do
+          position == 1 || position == 11 -> position + 2
+          position < 9 && rem(position - 1, 4) != 0 -> position + 4
+          true -> position
+        end
+      "R" ->
+        cond do
+          Enum.member?([2, 3, 5, 6, 7, 8, 10, 11], position) -> position + 1
+          true -> position
+        end
+      "L" ->
+        cond do
+          Enum.member?([3, 4, 6, 7, 8, 9, 11, 12], position) -> position - 1
+          true -> position
+        end
+      _ -> position
+    end
+  end
+
+  def parse_input(raw) do
+    String.split(raw, "\n")
+      |> Enum.map(fn(x) -> String.split(x, "") end)
   end
 end
--- test/day2_test.exs
+++ test/day2_test.exs
@@ -1,7 +1,218 @@
 defmodule Day2Test do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
-  test "parse_input creates a list with the different inputs" do
-    assert length(Day2.parse_input) == 5
+  describe "parse_input/0" do
+    test "creates a list 5 long" do
+      assert length(Day2.parse_input(ReadInput.from_day(2))) == 5
+    end
+  end
+
+  describe "move/2" do
+    test "1-U == 1" do
+      assert Day2.move(1, "U") == 1
+    end
+
+    test "1-D == 4" do
+      assert Day2.move(1, "D") == 4
+    end
+
+    test "1-L == 1" do
+      assert Day2.move(1, "L") == 1
+    end
+
+    test "1-R == 2" do
+      assert Day2.move(1, "R") == 2
+    end
+
+    test "2-U == 2" do
+      assert Day2.move(2, "U") == 2
+    end
+
+    test "2-D == 5" do
+      assert Day2.move(2, "D") == 5
+    end
+
+    test "2-L == 1" do
+      assert Day2.move(2, "L") == 1
+    end
+
+    test "2-R == 3" do
+      assert Day2.move(2, "R") == 3
+    end
+
+    test "3-U == 3" do
+      assert Day2.move(3, "U") == 3
+    end
+
+    test "3-D == 6" do
+      assert Day2.move(3, "D") == 6
+    end
+
+    test "3-L == 2" do
+      assert Day2.move(3, "L") == 2
+    end
+
+    test "3-R == 3" do
+      assert Day2.move(3, "R") == 3
+    end
+
+    test "4-U == 1" do
+      assert Day2.move(4, "U") == 1
+    end
+
+    test "4-D == 7" do
+      assert Day2.move(4, "D") == 7
+    end
+
+    test "4-L == 4" do
+      assert Day2.move(4, "L") == 4
+    end
+
+    test "4-R == 5" do
+      assert Day2.move(4, "R") == 5
+    end
+
+    test "5-U == 2" do
+      assert Day2.move(5, "U") == 2
+    end
+
+    test "5-D == 8" do
+      assert Day2.move(5, "D") == 8
+    end
+
+    test "5-L == 4" do
+      assert Day2.move(5, "L") == 4
+    end
+
+    test "5-R == 6" do
+      assert Day2.move(5, "R") == 6
+    end
+
+    test "6-U == 3" do
+      assert Day2.move(6, "U") == 3
+    end
+
+    test "6-D == 9" do
+      assert Day2.move(6, "D") == 9
+    end
+
+    test "6-L == 5" do
+      assert Day2.move(6, "L") == 5
+    end
+
+    test "6-R == 6" do
+      assert Day2.move(6, "R") == 6
+    end
+
+    test "7-U == 4" do
+      assert Day2.move(7, "U") == 4
+    end
+
+    test "7-D == 7" do
+      assert Day2.move(7, "D") == 7
+    end
+
+    test "7-L == 7" do
+      assert Day2.move(7, "L") == 7
+    end
+
+    test "7-R == 8" do
+      assert Day2.move(7, "R") == 8
+    end
+
+    test "8-U == 5" do
+      assert Day2.move(8, "U") == 5
+    end
+
+    test "8-D == 8" do
+      assert Day2.move(8, "D") == 8
+    end
+
+    test "8-L == 7" do
+      assert Day2.move(8, "L") == 7
+    end
+
+    test "8-R == 9" do
+      assert Day2.move(8, "R") == 9
+    end
+
+    test "9-U == 6" do
+      assert Day2.move(9, "U") == 6
+    end
+
+    test "9-D == 9" do
+      assert Day2.move(9, "D") == 9
+    end
+
+    test "9-L == 8" do
+      assert Day2.move(9, "L") == 8
+    end
+
+    test "9-R == 9" do
+      assert Day2.move(9, "R") == 9
+    end
+  end
+
+  describe "next_number/2" do
+    test "calculates the move when only one direction given" do
+      assert Day2.next_number(5, ["U"]) == 2
+    end
+
+    test "calculates multiple directions" do
+      assert Day2.next_number(5, ["U", "U", "L", "L", "D", "R"]) == 5
+    end
+  end
+
+  describe "code_list/2" do
+    test "returns result" do
+      assert Day2.code_list(5, [["U"], ["D"], ["L"], ["R"]]) == [2, 5, 4, 5]
+    end
+
+    test "returns result of first movement" do
+      assert Day2.code_list(5, [Enum.at(Day2.parse_input(ReadInput.from_day(2)), 0)]) == [3]
+    end
+
+    test "returns result of second movement" do
+      assert Day2.code_list(3, [Enum.at(Day2.parse_input(ReadInput.from_day(2)), 1)]) == [3]
+    end
+
+    test "returns result of 3rd movement" do
+      assert Day2.code_list(3, [Enum.at(Day2.parse_input(ReadInput.from_day(2)), 2)]) == [4]
+    end
+
+    test "returns result of 4th movement" do
+      assert Day2.code_list(4, [Enum.at(Day2.parse_input(ReadInput.from_day(2)), 3)]) == [4]
+    end
+
+    test "returns result of 5th movement" do
+      assert Day2.code_list(4, [Enum.at(Day2.parse_input(ReadInput.from_day(2)), 4)]) == [4]
+    end
+
+    test "returns result of input" do
+      assert Day2.code_list(5, Day2.parse_input(ReadInput.from_day(2))) == [3, 3, 4, 4, 4]
+    end
+
+    test "example" do
+      assert Day2.code_list(5, Day2.parse_input("ULL
+RRDDD
+LURDL
+UUUUD"
+      )) == [1, 9, 8, 5]
+    end
+
+    test "direct input" do
+      assert Day2.code_list(5, Day2.parse_input("RDLULDLDDRLLLRLRULDRLDDRRRRURLRLDLULDLDLDRULDDLLDRDRUDLLDDRDULLLULLDULRRLDURULDRUULLLUUDURURRDDLDLDRRDDLRURLLDRRRDULDRULURURURURLLRRLUDULDRULLDURRRLLDURDRRUUURDRLLDRURULRUDULRRRRRDLRLLDRRRDLDUUDDDUDLDRUURRLLUDUDDRRLRRDRUUDUUULDUUDLRDLDLLDLLLLRRURDLDUURRLLDLDLLRLLRULDDRLDLUDLDDLRDRRDLULRLLLRUDDURLDLLULRDUUDRRLDUDUDLUURDURRDDLLDRRRLUDULDULDDLLULDDDRRLLDURURURUUURRURRUUDUUURULDLRULRURDLDRDDULDDULLURDDUDDRDRRULRUURRDDRLLUURDRDDRUDLUUDURRRLLRR
+      RDRRLURDDDDLDUDLDRURRLDLLLDDLURLLRULLULUUURLDURURULDLURRLRULDDUULULLLRLLRDRRUUDLUUDDUDDDRDURLUDDRULRULDDDLULRDDURRUURLRRLRULLURRDURRRURLDULULURULRRLRLUURRRUDDLURRDDUUDRDLLDRLRURUDLDLLLLDLRURDLLRDDUDDLDLDRRDLRDRDLRRRRUDUUDDRDLULUDLUURLDUDRRRRRLUUUDRRDLULLRRLRLDDDLLDLLRDDUUUUDDULUDDDUULDDUUDURRDLURLLRUUUUDUDRLDDDURDRLDRLRDRULRRDDDRDRRRLRDULUUULDLDDDUURRURLDLDLLDLUDDLDLRUDRLRLDURUDDURLDRDDLLDDLDRURRULLURULUUUUDLRLUUUDLDRUDURLRULLRLLUUULURLLLDULLUDLLRULRRLURRRRLRDRRLLULLLDURDLLDLUDLDUDURLURDLUURRRLRLLDRLDLDRLRUUUDRLRUDUUUR
+      LLLLULRDUUDUUDRDUUURDLLRRLUDDDRLDUUDDURLDUDULDRRRDDLLLRDDUDDLLLRRLURDULRUUDDRRDLRLRUUULDDULDUUUDDLLDDDDDURLDRLDDDDRRDURRDRRRUUDUUDRLRRRUURUDURLRLDURDDDUDDUDDDUUDRUDULDDRDLULRURDUUDLRRDDRRDLRDLRDLULRLLRLRLDLRULDDDDRLDUURLUUDLLRRLLLUUULURUUDULRRRULURUURLDLLRURUUDUDLLUDLDRLLRRUUDDRLUDUDRDDRRDDDURDRUDLLDLUUDRURDLLULLLLUDLRRRUULLRRDDUDDDUDDRDRRULURRUUDLUDLDRLLLLDLUULLULLDDUDLULRDRLDRDLUDUDRRRRLRDLLLDURLULUDDRURRDRUDLLDRURRUUDDDRDUUULDURRULDLLDLDLRDUDURRRRDLDRRLUDURLUDRRLUDDLLDUULLDURRLRDRLURURLUUURRLUDRRLLULUULUDRUDRDLUL
+      LRUULRRUDUDDLRRDURRUURDURURLULRDUUDUDLDRRULURUDURURDRLDDLRUURLLRDLURRULRRRUDULRRULDLUULDULLULLDUDLLUUULDLRDRRLUURURLLUUUDDLLURDUDURULRDLDUULDDRULLUUUURDDRUURDDDRUUUDRUULDLLULDLURLRRLRULRLDLDURLRLDLRRRUURLUUDULLLRRURRRLRULLRLUUDULDULRDDRDRRURDDRRLULRDURDDDDDLLRRDLLUUURUULUDLLDDULDUDUUDDRURDDURDDRLURUDRDRRULLLURLUULRLUDUDDUUULDRRRRDLRLDLLDRRDUDUUURLRURDDDRURRUDRUURUUDLRDDDLUDLRUURULRRLDDULRULDRLRLLDRLURRUUDRRRLRDDRLDDLLURLLUDL
+      ULURLRDLRUDLLDUDDRUUULULUDDDDDRRDRULUDRRUDLRRRLUDLRUULRDDRRLRUDLUDULRULLUURLLRLLLLDRDUURDUUULLRULUUUDRDRDRUULURDULDLRRULUURURDULULDRRURDLRUDLULULULUDLLUURULDLLLRDUDDRRLULUDDRLLLRURDDLDLRLLLRDLDRRUUULRLRDDDDRUDRUULDDRRULLDRRLDDRRUDRLLDUDRRUDDRDLRUDDRDDDRLLRDUULRDRLDUDRLDDLLDDDUUDDRULLDLLDRDRRUDDUUURLLUURDLULUDRUUUDURURLRRDULLDRDDRLRDULRDRURRUDLDDRRRLUDRLRRRRLLDDLLRLDUDUDDRRRUULDRURDLLDLUULDLDLDUUDDULUDUDRRDRLDRDURDUULDURDRRDRRLLRLDLU"
+      )) == [3, 3, 4, 4, 4]
+    end
+  end
+
+  describe "leg1/0" do
+    test "returns result of day2 leg1" do
+      assert Day2.leg1 == "33444"
+    end
   end
 end
